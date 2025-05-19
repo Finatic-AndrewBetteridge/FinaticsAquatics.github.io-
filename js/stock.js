b7f097fa180a3f57e5b9459f9861d9b3ad7858a5
Finatic-AndrewBetteridge <atbetteridge@hotmail.co.uk~>
2025-05-19 09:23:32 +0100

    dev

--- js/stock.js
+++ js/stock.js
@@ -5,9 +5,13 @@ let stockData = {};
 function groupFishStock(data) {
   const grouped = {};
   data.forEach(({ fishName, size, price, stock, type = 'Uncategorized', category = 'General', subcategory = '', subcategory2 = '', subcategory3 = '' }) => {
-    const path = [type, category, subcategory, subcategory2, subcategory3].filter(Boolean).join(' > ');
-    grouped[path] = grouped[path] || {};
-    grouped[path][fishName] = grouped[path][fishName] || [];
+    const path = [type, category, subcategory, subcategory2, subcategory3]
+      .filter(Boolean)
+      .join(' > ');
+
+    if (!grouped[path]) grouped[path] = {};
+    if (!grouped[path][fishName]) grouped[path][fishName] = [];
+
     grouped[path][fishName].push({ size, price, stock });
   });
   return grouped;
