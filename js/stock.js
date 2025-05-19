3e7280f5d061509d15791872c64d176431aaa099
Finatic-AndrewBetteridge <atbetteridge@hotmail.co.uk~>
2025-05-19 09:07:37 +0100

    dev

--- js/stock.js
+++ js/stock.js
@@ -14,11 +14,16 @@ function groupFishStock(data) {
 }
 
 function fetchStock() {
-  return fetch(sheetUrl)
-    .then(res => res.json())
+  const sheetUrl = 'https://script.google.com/macros/s/AKfycby7R9zrOBS-pg0AwxU_yRaKLo6VUWM8oPjLFkZhiJyl2SkTVw98ENSsO3iC3ISHYqSd/exec';
+
+  fetch(sheetUrl)
+    .then(res => {
+      if (!res.ok) throw new Error(`Fetch error: ${res.status}`);
+      return res.json();
+    })
     .then(data => {
       stockData = groupFishStock(data);
       renderFishGrid();
     })
-    .catch(err => console.error('Failed to fetch stock:', err));
+    .catch(err => console.error('❌ Failed to fetch stock:', err));
 }
