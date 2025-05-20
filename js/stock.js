176b9fbc539a6ff0c1262c8c6ff350f5b5b11b62
Finatic-AndrewBetteridge <atbetteridge@hotmail.co.uk~>
2025-05-20 11:21:13 +0100

    dev

--- js/stock.js
+++ js/stock.js
@@ -5,7 +5,9 @@ let stockData = {};
 function groupFishStock(data) {
   const grouped = {};
   data.forEach(({ fishName, size, price, salePrice, stock, type = 'Uncategorized', category = 'General', subcategory = '', subcategory2 = '', subcategory3 = '' }) => {
-    const path = [type, category, subcategory, subcategory2, subcategory3].filter(Boolean).join(' > ');
+    const path = [type, category, subcategory, subcategory2, subcategory3]
+      .filter(Boolean)
+      .join(' > ');
 
     if (!grouped[path]) grouped[path] = {};
     if (!grouped[path][fishName]) grouped[path][fishName] = [];
@@ -16,14 +18,20 @@ function groupFishStock(data) {
 }
 
 function fetchStock() {
-  return fetch(stockUrl)  // must return the fetch promise!
+  return fetch(stockUrl)
     .then(res => {
       if (!res.ok) throw new Error(`Fetch error: ${res.status}`);
       return res.json();
     })
     .then(data => {
       stockData = groupFishStock(data);
+
+      // Ensure renderFishGrid is defined before calling
+      if (typeof renderFishGrid !== 'function') {
+        console.error("❌ renderFishGrid is not defined. Make sure RenderFishCard.js is loaded before stock.js.");
+        return;
+      }
+
       renderFishGrid();
     });
 }
-
